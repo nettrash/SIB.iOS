544d10e3c086aafef029e78af429408bd53bc6cb
NETTRASH <nettrash@nettrash.ru>
2017-09-23 11:49:41 +0300

    Initial Commit

---
+++ sib-lite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sib-lite
+//
+//  Created by Иван Алексеев on 23.09.17.
+//  Copyright © 2017 NETTRASH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ sib-liteTests/sib_liteTests.swift
@@ -0,0 +1,36 @@
+//
+//  sib_liteTests.swift
+//  sib-liteTests
+//
+//  Created by Иван Алексеев on 23.09.17.
+//  Copyright © 2017 NETTRASH. All rights reserved.
+//
+
+import XCTest
+@testable import sib_lite
+
+class sib_liteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
